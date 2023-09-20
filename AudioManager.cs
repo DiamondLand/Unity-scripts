8b3c871dec5d716cacf3fe05966ce7882107f36f
DiamondLand <simoforstudio@gmail.com>
2023-09-20 13:01:49 +0300

    music
    Система воспроизведения музыки

---
+++ AudioManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public void PlayAudio(AudioClip clip)
+    {
+        GetComponent<AudioSource>().PlayOneShot(clip);
+    }
+}

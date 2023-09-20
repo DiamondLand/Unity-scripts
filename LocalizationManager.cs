7791ccd5ce53ea5784d2c9f6bf9e57b78fab5969
DiamondLand <simoforstudio@gmail.com>
2023-09-20 13:02:31 +0300

    localization
    Система смены языков игры

---
+++ LocalizationManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LocalizationManager : MonoBehaviour
+{
+    public void language_Rus()
+    {
+        string language = "Rus";
+        PlayerPrefs.SetString("Language", language);
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1;
+    }
+
+    public void language_Eng()
+    {
+        string language = "Eng";
+        PlayerPrefs.SetString("Language", language);
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1;
+    }
+    
+}
---
+++ TextItem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextItem : MonoBehaviour
+{
+  public string language;
+  Text text;
+
+  public string textRus;
+  public string textEng;
+
+  void Start()
+  {
+      text = GetComponent<Text>();
+      language = PlayerPrefs.GetString("Language");
+
+      if (language == "" || language == "Rus")
+      {
+        text.text = textRus;
+      }
+
+      else if (language == "Eng")
+      {
+        text.text = textEng;
+      }
+
+  }
+}
